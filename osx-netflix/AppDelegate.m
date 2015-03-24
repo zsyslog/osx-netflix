7a9aaeadbc5b984c833f1501c5ca7c91524b341b
Juan Carlos <jcjj.system@gmail.com>
2015-03-24 15:20:47 -0300

    refactor

--- osx-netflix/AppDelegate.m
+++ osx-netflix/AppDelegate.m
@@ -13,15 +13,13 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     
     _webView.CustomUserAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/600.4.10 (KHTML, like Gecko) Version/8.0.4 Safari/600.4.10";
+    _webView.FrameLoadDelegate = self;
     
-    [_webView setFrameLoadDelegate:self];
-    
+    // Load Netflix URL
     NSURL *url = [NSURL URLWithString:@"http://www.netflix.com/Login"];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     [[[self webView] mainFrame] loadRequest:urlRequest];
     
-    
-    //   NSRect screenRect = NSMakeRect(0.f, 0.f, 1240.f, 830.f); 
     // ---- Detect screen size ----- //
     NSRect screenRect;
     NSArray *screenArray = [NSScreen screens];
@@ -30,25 +28,19 @@
         NSScreen *screen = [screenArray objectAtIndex: index];
         screenRect = [screen visibleFrame];
     }
+    // NSRect screenRect = NSMakeRect(0.f, 0.f, 1240.f, 830.f);
     
-    // ----- Set window size ---- //
-//    [[_window standardWindowButton:NSWindowCloseButton] setHidden:YES];
-//    [[_window standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
-//    [[_window standardWindowButton:NSWindowZoomButton] setHidden:YES];
+    // ----- Set window properties ---- //
+    // [[_window standardWindowButton:NSWindowCloseButton] setHidden:YES];
+    // [[_window standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+    // [[_window standardWindowButton:NSWindowZoomButton] setHidden:YES];
     [_window setFrame:screenRect display:YES animate:YES];
-    [_window setContentView:_webView];
+    [_window setContentView:_webView]; // <---- _window.ContentView = _webView;
 
-    //NSRect webFrameRect = [[[webFrame frameView] documentView] frame];
-    //get the rect of the current webview
-    //NSRect webViewRect = [webview frame];
+    // calculate the WebView size
+    NSRect newWebViewRect = NSMakeRect(0, 0, NSWidth(screenRect), NSHeight(screenRect));
     
-    //calculate the new frame
-    NSRect newWebViewRect = NSMakeRect(0,
-                                       0,
-                                       NSWidth(screenRect),
-                                       NSHeight(screenRect));
-    
-    [_webView setFrame:newWebViewRect];
+    _webView.Frame = newWebViewRect;
     
     // NSLog(@"Rect: %@",NSStringFromRect(_webView.frame));
     
